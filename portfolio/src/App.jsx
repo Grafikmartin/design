7aa44e2b8f1808a5667258a1b93caa696df93bd3
Grafikmartin <grafikmartin@gmail.com>
2026-01-20 20:05:04 +0100

    Initial commit: Portfolio-Seite mit React und Vite

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <main className="welcome-container">
+        <h1 className="welcome-text">WELCOME TO MY CORNER OF THE WEB</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
